368b97f81c7545dff23ade95cb5e544709c64936
ceastld <1950946176@qq.com>
2025-01-23 04:13:51 +0000

    env

---
+++ create_env.sh
@@ -0,0 +1,11 @@
+conda deactivate
+conda env remove -n liveportrait -y
+ml cuDNN/8.7.0.84-CUDA-11.8.0
+conda create -n liveportrait python=3.10 -y
+conda activate liveportrait
+# pip3 install torch torchvision torchaudio
+pip install torch==2.3.1 torchvision==0.18.1 torchaudio==2.3.1 --index-url https://download.pytorch.org/whl/cu118
+pip install -r requirements.txt
+pip install onnxruntime-gpu==1.18.0
+
+huggingface-cli download KwaiVGI/LivePortrait --local-dir pretrained_weights --exclude "*.git*" "README.md" "docs"
